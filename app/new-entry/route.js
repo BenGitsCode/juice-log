7bb37b8583db81b020637bb22dde4c68945d3e3d
benjamski <benjadamski@gmail.com>
2016-09-28 14:41:29 -0400

    Mark new branch
    new entry feature incomplete

--- app/new-entry/route.js
+++ app/new-entry/route.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+
+  actions: {
+    newEntry (details) {
+      this.get('auth').newEntry(details)
+      .then(() => this.get('auth').newEntry(details))
+      .then(() => this.transitionTo('application'))
+      .then(() => {
+        this.get('flashMessages')
+        .success('Successfully created new entry');
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('Uh oh, dry fire. There was a problem. Please try again.');
+      });
+    },
+  },
 });
